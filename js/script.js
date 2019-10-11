2abc32ca64d4288fdcd82ca67a190b21317a57dd
Bett Baranovsky <black.laufer2000@gmail.com>
2019-10-11 10:19:15 +0200

    Second tasc js/script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
   
@@ -23,7 +24,12 @@ const titleClickHandler = function(event){
       activeArticle.classList.remove('active');
   }
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = function('href'); 
+
+    console.log(articleSelector);
+
+    
+
     /* find the correct article using the selector (value of 'href' attribute) */
   
     /* add class 'active' to the correct article */
